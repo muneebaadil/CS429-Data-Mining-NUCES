4ea84cdb39397af8599ad4faa9a4393cdc377336
Muneeb Aadil <imuneebaadil@gmail.com>
2017-03-10 01:42:48 +0500

    Scraped ads list

--- Assignments/scrapyTask/scrapyTask/spiders/OLXcarscars.py
+++ Assignments/scrapyTask/scrapyTask/spiders/OLXcarscars.py
@@ -27,7 +27,20 @@ class OlxcarscarsSpider(scrapy.Spider):
     	cats = soup.find_all(findcategories)
     	for c in cats: 
     		nexturl = c.parent['href']
+    		nexturl = nexturl + '?search[photos]=false'
     		yield scrapy.Request(nexturl, callback=self.parseList)
 
     def parseList(self, response):
+    	html = response.body
+    	soup = BeautifulSoup(html, 'lxml')
+
+    	container = soup.find('div', id='listContainer')
+    	section = container.find('section', id='body-container')	
+    	ads = section.find_all('td', class_='offer onclick ')
+
+    	for ad in ads:
+    		nexturl = ad.find('a', href=True)['href']
+    		yield scrapy.Request(nexturl, callback=self.parseAd) 
+
+    def parseAd(self, response):
     	pass
