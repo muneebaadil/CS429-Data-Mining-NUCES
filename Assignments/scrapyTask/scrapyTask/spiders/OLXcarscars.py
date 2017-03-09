71256bcda601934cf7feebffd113991175bf9f33
Muneeb Aadil <imuneebaadil@gmail.com>
2017-03-09 23:32:56 +0500

    OLX home page scraping done!

--- Assignments/scrapyTask/scrapyTask/spiders/OLXcarscars.py
+++ Assignments/scrapyTask/scrapyTask/spiders/OLXcarscars.py
@@ -1,25 +1,22 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
+from bs4 import BeautifulSoup
 
 class OlxcarscarsSpider(scrapy.Spider):
     name = "OLXcarscars"
     #allowed_domains = ["olx.com.pk/cars/"]
     start_urls = (
-        'http://www.olx.com.pk/cars/',
+        'http://www.olx.com.pk/',
     )
 
+    #Home page (olx.com.pk) parser to get different categories pages.
     def parse(self, response):
-        urlpieces = response.url.split('=')
-        pagenum = '1' if (len(urlpieces) == 1) else urlpieces[-1]
+        html = response.body
+        soup = BeautifulSoup(html, 'lxml')
+        tds = soup.find_all(name='td')
+        for tag in tds: 
+        	nexturl = tag.find(name='a', href=True)['href']
+        	yield scrapy.Request(nexturl, callback=self.parseCategory)
 
-        with open('./OLXcarscars/olxcars%s.html' % pagenum, 'w') as f: 
-        	f.write(response.body)
-        
-        basexpath = '/html/body/div[1]/div[1]/section[2]/div[1]/div/div[2]'
-        basexpath = '/html/body/div[1]/div[1]/section[2]/div[1]/div/div[2]'
-        nexturlxpath = basexpath + '/span[' + str(int(pagenum)+2) + ']/a/@href'
-        nexturl = response.xpath(nexturlxpath).extract_first()
-
-        print 'NEXTURL =', nexturl
-        yield scrapy.Request(nexturl)
+    def parseCategory(self, response):
+    	pass
