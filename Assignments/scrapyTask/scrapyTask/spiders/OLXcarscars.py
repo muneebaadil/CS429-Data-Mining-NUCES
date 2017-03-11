171ead977b8111b02e6fe83ac7d465027647bd21
Muneeb Aadil <imuneebaadil@gmail.com>
2017-03-11 15:52:44 +0500

    category scraped too! Submission stage.

--- Assignments/scrapyTask/scrapyTask/spiders/OLXcarscars.py
+++ Assignments/scrapyTask/scrapyTask/spiders/OLXcarscars.py
@@ -6,10 +6,6 @@ import re
 def findcategories(tag):
     return unicode(tag.string).find('View all Ads') != -1 and tag.name == 'span'
 
-def mainbox(tag):
-	return tag.name == 'div' and tag.has_attr('class') and \
-	('clr' in tag['class']) and ('offerbody' in tag['class'])
-
 class OlxscraperSpider(scrapy.Spider):
     name = "OLXscraper"
     #allowed_domains = ["olx.com.pk/cars/"]
@@ -17,18 +13,26 @@ class OlxscraperSpider(scrapy.Spider):
         'http://www.olx.com.pk/',
     )
 
-    #Home page (olx.com.pk) parser to get different categories pages.
     def parse(self, response):
+    	"""
+    	Home page (olx.com.pk) parser to get to a page containing multiple 
+    	categories.
+    	"""
         html = response.body
         soup = BeautifulSoup(html, 'lxml')
+
         tag = soup.find(name='td')
         nexturl = tag.find(name='a', href=True)['href']
         yield scrapy.Request(nexturl, callback=self.parseCategory)
-
-    
+ 
     def parseCategory(self, response):
+    	"""
+		Category page parser. It finds out each category ads and forwards
+		each category-related ads list to parseList() 
+    	"""
     	html = response.body
     	soup = BeautifulSoup(html, 'lxml')
+    	
     	cats = soup.find_all(findcategories)
     	for c in cats: 
     		nexturl = c.parent['href']
@@ -36,24 +40,48 @@ class OlxscraperSpider(scrapy.Spider):
     		yield scrapy.Request(nexturl, callback=self.parseList)
 
     def parseList(self, response):
+    	"""
+		Parses a list page; forwards each ad on the list to parseAd(); and 
+		recursively calls itself on the next list page. 
+    	"""
+    	#Extracting html off the response and making its soup.. 
     	html = response.body
     	soup = BeautifulSoup(html, 'lxml')
 
+    	#Getting a list of ads' tags. 
     	container = soup.find('div', id='listContainer')
     	section = container.find('section', id='body-container')	
     	ads = section.find_all('td', class_='offer onclick ')
 
     	for ad in ads:
+    		#Generating a new request to parse an individual ad. 
     		nexturl = ad.find('a', href=True)['href']
-    		yield scrapy.Request(nexturl, callback=self.parseAd) 
+    		category = container.section.div.div.div.find_all('li')[-1].h1.string
+    		
+    		req = scrapy.Request(nexturl, callback=self.parseAd) 
+    		#Indirectly passing an argument to parseAd() 
+    		req.meta['category'] = category
+    		yield req 
+
+    	#Extacting the next page's link and recursively calling itself
+    	#on next page's list (if there's any). 
+    	pagertag = soup.find(name='div', class_='pager')
+    	nextpagetag = pagertag.find(name='span', class_=['next'])
+    	if nextpagetag is not None: 
+    		nexturl = nextpagetag.a['href']
+    		yield scrapy.Request(nexturl, callback=self.parseList)
 
     def parseAd(self, response):
+    	"""
+		Finally, parse an individual ad and stores its information. 
+    	"""
+    	#Extracting html off response and making its soup
     	html = response.body 
     	soup = BeautifulSoup(html, 'lxml')
     	
     	#Dividing the page into sections for future convenience.
-    	mainsec = soup.find(mainbox)
-    	left, right = mainsec.find_all(name='div', recursive=False)
+    	left = soup.find(name='div', class_=['offercontent'])
+    	right = soup.find(name='div', class_=['offerbox'], id=['offerbox'])
     	
     	#Left section attributes. 
     	title = unicode(left.find(name='h1', class_=True).string)
@@ -79,6 +107,7 @@ class OlxscraperSpider(scrapy.Spider):
     	'views': views,
     	'price': price, 
     	"user name": user, 
-    	'contact': contact 
+    	'contact': contact,
+    	'category': response.meta['category']
     	}
     	return 
