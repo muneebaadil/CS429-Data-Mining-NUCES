34bc01055006beb5c52783d3957beb73b1f89b3a
Dr fraud <Dr fraud>
2017-04-25 05:17:23 +0500

    Assignment 06 init

---
+++ Assignments/Assignment06/logisiticregression.py
@@ -0,0 +1,123 @@
+#---------------------------------------------#
+#-------| Written By: Sibt ul Hussain |-------#
+#---------------------------------------------#
+
+# A Logistic Regression algorithm with regularized weights...
+
+from classifier import *
+
+#Note: Here the bias term is considered as the last added feature 
+
+class LogisticRegression(Classifier):
+    ''' Implements the LogisticRegression For Classification... '''
+    def __init__(self, lembda=0.001):        
+        """
+            lembda= Regularization parameter...            
+        """
+        Classifier.__init__(self,lembda)                
+        
+        pass
+    def sigmoid(self,z):
+        """
+            Compute the sigmoid function 
+            Input:
+                z can be a scalar or a matrix
+            Returns:
+                sigmoid of the input variable z
+        """
+
+        # Your Code here
+        return        
+    
+    
+    def hypothesis(self, X,theta):
+        '''
+            Computes the hypothesis for over given input examples (X) and parameters (thetas).
+
+            Input:
+                X: can be either a single n X d-dimensional vector or n X d dimensional matrix
+                theta: Must be a d-dimensional vector
+            Return:
+                The computed hypothesis
+        '''
+        
+        # Your Code here
+        return
+
+        
+    def cost_function(self, X,Y, theta):
+        '''
+            Computes the Cost function for given input data (X) and labels (Y).
+
+            Input:
+                X: can be either a single n X d-dimensional vector or n X d dimensional matrix of inputs
+                theata: must  d X 1-dimensional vector for representing vectors
+                Y: Must be n X 1-dimensional label vector
+                
+            Return:
+                Returns the cost of hypothesis with input parameters 
+        '''
+    
+    
+        # Your Code here
+        return
+
+    def derivative_cost_function(self,X,Y,theta):
+        '''
+            Computes the derivates of Cost function w.r.t input parameters (thetas)  
+            for given input and labels.
+
+            Input:
+            ------
+                X: can be either a single n X d-dimensional vector or n X d dimensional matrix of inputs
+                theata: must  d X 1-dimensional vector for representing vectors
+                Y: Must be n X 1-dimensional label vector
+            Returns:
+            ------
+                partial_thetas: a d X 1-dimensional vector of partial derivatives of cost function w.r.t parameters..
+        '''
+        
+        # Your Code here
+        return
+
+    def train(self, X, Y, optimizer):
+        ''' Train classifier using the given 
+            X [m x d] data matrix and Y labels matrix
+            
+            Input:
+            ------
+            X: [m x d] a data matrix of m d-dimensional examples.
+            Y: [m x 1] a label vector.
+            optimizer: an object of Optimizer class, used to find
+                       find best set of parameters by calling its
+                       gradient descent method...
+            Returns:
+            -----------
+            Nothing
+            '''
+        
+        # Your Code here 
+        # Use optimizer here
+
+        return
+    
+    def predict(self, X):
+        
+        """
+        Test the trained perceptron classifier result on the given examples X
+        
+                   
+            Input:
+            ------
+            X: [m x d] a matrix of m  d-dimensional test examples.
+           
+            Returns:
+            -----------
+                pclass: the predicted class for the given set of examples, i.e. to which it belongs
+        """
+        
+        num_test = X.shape[0]
+        
+        
+        # Your Code here
+        return
---
+++ Assignments/Assignment06/perceptron.py
@@ -0,0 +1,108 @@
+# ---------------------------------------------#
+# -------| Written By: Sibt ul Hussain |-------#
+# ---------------------------------------------#
+
+# A Perceptron algorithm with regularized weights...
+
+from classifier import *
+#Note: Here the bias term is considered as the last added feature
+
+# Note: Here the bias term is being considered as the last added feature
+class Perceptron(Classifier):
+    ''' Implements the Perceptron inherited from Classifier For Classification... '''
+
+    def __init__(self, lembda=0):
+        """
+            lembda= Regularization parameter...
+        """
+
+        Classifier.__init__(self, lembda)
+
+        pass
+
+    def hypothesis(self, X, theta):
+        '''
+            Computes the hypothesis for over given input examples (X) and parameters (thetas).
+
+            Input:
+                X: can be either a single n X d-dimensional vector or n X d dimensional matrix
+                theta: Must be a d-dimensional vector
+            Return:
+                The computed hypothesis
+        '''
+        
+        # Your Code here
+        return
+
+    def cost_function(self, X, Y, theta):
+        '''
+            Computes the Cost function for given input data (X) and labels (Y).
+
+            Input:
+                X: can be either a single n X d-dimensional vector or n X d dimensional matrix of inputs
+                theata: must  d X 1-dimensional vector for representing vectors
+                Y: Must be n X 1-dimensional label vector
+                
+            Return:
+                Returns the cost of hypothesis with input parameters 
+        '''
+
+        
+        # Your Code here
+        return
+
+
+    def derivative_cost_function(self, X, Y, theta):
+        '''
+            Computes the derivates of Cost function w.r.t input parameters (thetas)  
+            for given input and labels.
+
+            Input:
+            ------
+                X: can be either a single n X d-dimensional vector or n X d dimensional matrix of inputs
+                theata: must  d X 1-dimensional vector for representing vectors
+                Y: Must be n X 1-dimensional label vector
+            Returns:
+            ------
+                partial_thetas: a d X 1-dimensional vector of partial derivatives of cost function w.r.t parameters..
+        '''
+        
+        # Your Code here
+        return
+
+    def train(self, X, Y, optimizer):
+        ''' Train classifier using the given 
+            X [m x d] data matrix and Y labels matrix
+            
+            Input:
+            ------
+            X: [m x d] a data matrix of m d-dimensional examples.
+            Y: [m x 1] a label vector.
+            optimizer: an object of Optimizer class, used to find
+                       find best set of parameters by calling its
+                       gradient descent method...
+            Returns:
+            -----------
+            Nothing
+            '''
+
+        nexamples, nfeatures = X.shape
+        # Your Code here
+        return
+
+    def predict(self, X):
+        """
+        Test the trained perceptron classifier result on the given examples X
+        
+                   
+            Input:
+            ------
+            X: [m x d] a matrix of m  d-dimensional test examples.
+           
+            Returns:
+            -----------
+            pclass: the predicted class for the given set of examples, i.e. to which it belongs
+        """
+
+        # Your Code here
+        return
