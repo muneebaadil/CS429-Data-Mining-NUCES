140d22920d508fa4a3b5bf12ba295785b76f280e
Muneeb Aadil <imuneebaadil@gmail.com>
2017-05-27 16:33:30 +0500

    Added SaveOnKaggleFormat() method

--- Project/newutils.py
+++ Project/newutils.py
@@ -90,47 +90,6 @@ def ConstructTrainingSet(setname):
             
     pass
 
-def ConstructTestDay(ofname, wfname, clusterfname, poifname):
-    orderdf=pd.read_csv(ofname,sep=' ',header=None)
-    clustermapdf = pd.read_csv(clusterfname, sep='\t', 
-                                   names=['RegionHash', 'RegionID'])
-    weatherdf = pd.read_csv(wfname, sep='\t', header=None, na_filter=False, 
-                         names=['Time', 'Weather', 'Temperature', 'PM2.5'])
-    poidf = ConstructPOITable(poifname).reset_index()
-    poidf = pd.merge(poidf,clustermapdf,on='RegionHash',how='left')
-
-    orderdf['RegionHash'] = orderdf[0].apply(getregid)
-    orderdf['Date'] = orderdf[0].apply(getdate)
-    orderdf['Timeslot'] = orderdf[1].apply(totimeslot)
-    orderdf.drop([0,1],axis=1,inplace=True)
-
-    orderdf = pd.merge(orderdf,clustermapdf,on='RegionHash',how='left').drop('RegionHash',axis=1)
-    weatherdf['Timeslot'] = weatherdf['Time'].apply(totimeslot)
-    orderdf=pd.merge(orderdf,weatherdf[['Timeslot','Weather','Temperature','PM2.5']],on='Timeslot',how='left')
-    
-    temp1=orderdf.groupby(['RegionID','Timeslot','Date'])['Weather','Temperature','PM2.5'].mean().reset_index()
-    temp2=orderdf.groupby(['RegionID','Timeslot','Date'])['Weather'].agg({'Demand':lambda x:int(x.shape[0])}).reset_index()
-    orderdf=pd.merge(temp1,temp2,on=['RegionID','Timeslot','Date'],how='inner')
-    
-    orderdf = pd.merge(orderdf,poidf,on='RegionID',how='left').drop('RegionHash',axis=1)
-    orderdf.to_csv(np.unique(orderdf.Date.values)[0]+'.csv',index=False)
-    return orderdf
-
-def ConstructTestSet(setname):
-    ofnames = [setname+'order_data/'+x for x in sorted(os.listdir('./test_set/order_data/')) if x[0]!='.']
-    wfnames = [setname+'weather_data/'+x for x in sorted(os.listdir('./test_set/weather_data/')) if x[0]!='.']
-    poifname = setname + 'poi_data/poi_data'
-    clustermapfname = setname + 'cluster_map/cluster_map'
-    
-    print 'files done =',
-    i=1
-    for ofname, wfname in zip(ofnames, wfnames):
-        ConstructTestDay(ofname, wfname, clustermapfname, poifname)
-        print str(i)+',',
-        i+=1
-            
-    pass
-
 def LoadTrainingSet(dirname): 
     fnames = [dirname+x for x in sorted(os.listdir(dirname))]
     Xs,Ys = [],[]
@@ -158,3 +117,78 @@ def SplitTrainingSet(X,Y,fraction):
     Yval = Y.iloc[splitpoint:]
     
     return Xtrain,Ytrain,Xval,Yval
+
+def ConstructTestSet(setname):
+    ofnames = [setname+'order_data/'+x for x in sorted(os.listdir('./test_set/order_data/')) if x[0]!='.']
+    wfnames = [setname+'weather_data/'+x for x in sorted(os.listdir('./test_set/weather_data/')) if x[0]!='.']
+    poifname = setname + 'poi_data/poi_data'
+    clustermapfname = setname + 'cluster_map/cluster_map'
+    
+    print 'files done =',
+    i=1
+    for ofname, wfname in zip(ofnames, wfnames):
+        ConstructTestDay(ofname, wfname, clustermapfname, poifname)
+        print str(i)+',',
+        i+=1
+            
+    pass
+
+def ConstructTestDay(ofname, wfname, clusterfname, poifname):
+    orderdf=pd.read_csv(ofname,sep=' ',header=None)
+    clustermapdf = pd.read_csv(clusterfname, sep='\t', 
+                                   names=['RegionHash', 'RegionID'])
+    weatherdf = pd.read_csv(wfname, sep='\t', header=None, na_filter=False, 
+                         names=['Time', 'Weather', 'Temperature', 'PM2.5'])
+    poidf = ConstructPOITable(poifname).reset_index()
+    poidf = pd.merge(poidf,clustermapdf,on='RegionHash',how='left')
+
+    orderdf['RegionHash'] = orderdf[0].apply(getregid)
+    orderdf['Date'] = orderdf[0].apply(getdate)
+    orderdf['Timeslot'] = orderdf[1].apply(totimeslot)
+    orderdf.drop([0,1],axis=1,inplace=True)
+
+    orderdf = pd.merge(orderdf,clustermapdf,on='RegionHash',how='left').drop('RegionHash',axis=1)
+    weatherdf['Timeslot'] = weatherdf['Time'].apply(totimeslot)
+    orderdf=pd.merge(orderdf,weatherdf[['Timeslot','Weather','Temperature','PM2.5']],on='Timeslot',how='left')
+    
+    temp1=orderdf.groupby(['RegionID','Timeslot','Date'])['Weather','Temperature','PM2.5'].mean().reset_index()
+    temp2=orderdf.groupby(['RegionID','Timeslot','Date'])['Weather'].agg({'Demand':lambda x:int(x.shape[0])}).reset_index()
+    orderdf=pd.merge(temp1,temp2,on=['RegionID','Timeslot','Date'],how='inner')
+    
+    orderdf = pd.merge(orderdf,poidf,on='RegionID',how='left').drop('RegionHash',axis=1)
+    orderdf.to_csv(np.unique(orderdf.Date.values)[0]+'.csv',index=False)
+    return orderdf
+
+
+def LoadTestSet(dirname):
+    fnames = [dirname+x for x in sorted(os.listdir(dirname))]
+    Xs,Ys = [],[]
+    for fname in fnames: 
+        csvfile=pd.read_csv(fname)
+        Xs.append(csvfile)
+    X = pd.concat(Xs,ignore_index=True)
+    X = X[[u'Date', u'RegionID', u'Timeslot', u'Demand', u'Weather',
+       u'Temperature', u'PM2.5', u'F1', u'F24', u'F25', u'F20', u'F22', u'F23',
+       u'F4', u'F8', u'F5', u'F14', u'F7', u'F15', u'F16', u'F17', u'F11',
+       u'F13', u'F19', u'F6', u'F3', u'F2', u'F21', u'F12', u'F18', u'F9',
+       u'F10']]
+    return X
+    
+def SaveInKaggleFormat(df, fin, fout):
+    clusterfname = './test_set/cluster_map/cluster_map'
+    clustermapdf = pd.read_csv(clusterfname, sep='\t', 
+                                   names=['RegionHash', 'RegionID'])
+    df = pd.merge(df,clustermapdf,on='RegionID',how='left')
+    
+    myout = df[['Date','RegionHash','Timeslot','gap']]
+    myout['id'] = myout.RegionHash.astype(str)+'_'+myout.Date.astype(str)+'_'+(myout['Timeslot']-1).astype(str)
+    myout.drop(['Date','RegionHash','Timeslot'],axis=1,inplace=True)
+    
+    kgout = pd.read_csv(fin)
+    kgout['gap']=0.
+    
+    myout = pd.merge(kgout, myout, on='id', how='left')
+    myout = myout.drop('gap_x',axis=1).rename(columns={'gap_y': 'gap'}).fillna(0)
+    
+    myout.to_csv(fout, index=False)
+    return myout
